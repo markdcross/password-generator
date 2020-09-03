579aff6132ed9962eb15deddd4b3efd5d59f7c03
MDC <markdcross@gmail.com>
2020-09-03 18:14:39 -0400

    Fixed password reset on new password generation

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var passwordText = document.querySelector('#password');
 var copyBtn = document.querySelector('#copy');
 var password = '';
 
-// Function to create and display password when button is clicked
+// Creates and displays password when button is clicked
 function writePassword() {
+    password = '';
+    // Asks users the criteria questions and records answers
     questions();
 
+    // Uses the information collected in Questions to generate a random password based on the selected criteria
     generatePassword();
 
+    // Returns password to text area
     passwordText.value = password;
 }
 
@@ -32,6 +36,7 @@ function questions() {
     if (length < 8 || length > 128) {
         alert('Password length must be between 8-128 characters');
         questions();
+        return;
     }
 
     // Confirms for types (lowercase, uppercase, numeric, special)
@@ -52,22 +57,22 @@ function questions() {
 }
 
 // Functions to generate random characters for each type selected by user
-// Function to get lowercase letters
+// Gets lowercase letters
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-// Function to get uppercase letters
+// Gets uppercase letters
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-// Function to get numbers
+// Gets numbers
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-// Function to get special characters
+// Gets special characters
 function getRandomSpecial() {
     const specialChar = '!@#$%^&*()_+';
     return specialChar[Math.floor(Math.random() * specialChar.length)];
@@ -76,16 +81,16 @@ function getRandomSpecial() {
 // Populate password character array based on user input
 function passGen(lowercase, uppercase, number, special) {
     var passwordArr = [];
-    if (lowercase === true) {
+    if (lowercase) {
         passwordArr.push([getRandomLower()]);
     }
-    if (uppercase === true) {
+    if (uppercase) {
         passwordArr.push([getRandomUpper()]);
     }
-    if (number === true) {
+    if (number) {
         passwordArr.push([getRandomNumber()]);
     }
-    if (special === true) {
+    if (special) {
         passwordArr.push([getRandomSpecial()]);
     }
     return passwordArr[Math.floor(Math.random() * passwordArr.length)];
@@ -93,7 +98,7 @@ function passGen(lowercase, uppercase, number, special) {
 
 // Loop the character generation over the length of the password
 function generatePassword() {
-    for (i = 0; i < length; i++) {
+    for (var i = 0; i < length; i++) {
         password += passGen(lowercase, uppercase, number, special);
     }
 }
