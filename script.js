de3f3a9b90b3cb47b3a97c1b07cc618f28b5f18f
MDC <markdcross@gmail.com>
2020-09-04 08:28:03 -0400

    Reordering script.js to ensure proper order of operations

--- script.js
+++ script.js
@@ -4,29 +4,7 @@ var passwordText = document.querySelector('#password');
 var copyBtn = document.querySelector('#copy');
 var password = '';
 
-// Creates and displays password when button is clicked
-function writePassword() {
-    password = '';
-    // Asks users the criteria questions and records answers
-    questions();
-
-    // Uses the information collected in Questions to generate a random password based on the selected criteria
-    generatePassword();
-
-    // Returns password to text area
-    passwordText.value = password;
-}
-
-function copyPassword() {
-    var copyText = document.getElementById('password');
-    copyText.select();
-    document.execCommand('copy');
-    alert('Copied the text: ' + copyText.value);
-}
-
-// Internal functions that power writePassword()
-
-// Questions to determine what password criteria to use
+// Questions are asked via a prompt and several confirms to determine what password criteria to use
 function questions() {
     // Prompt for length of password (8-128)
     length = +prompt(
@@ -36,14 +14,19 @@ function questions() {
     if (length < 8 || length > 128) {
         alert('Password length must be between 8-128 characters');
         questions();
-        return;
     }
 
     // Confirms for types (lowercase, uppercase, numeric, special)
-    lowercase = confirm('Do you want lowercase letters?');
-    uppercase = confirm('Do you want uppercase letters?');
-    number = confirm('Do you want numbers?');
-    special = confirm('Do you want special characters?');
+    lowercase = confirm(
+        'Do you want lowercase letters (OK for yes, Cancel for no)?'
+    );
+    uppercase = confirm(
+        'Do you want uppercase letters (OK for yes, Cancel for no)?'
+    );
+    number = confirm('Do you want numbers (OK for yes, Cancel for no)?');
+    special = confirm(
+        'Do you want special characters (OK for yes, Cancel for no)?'
+    );
     // Validation
     if (
         lowercase === false &&
@@ -56,22 +39,19 @@ function questions() {
     }
 }
 
-// Functions to generate random characters for each type selected by user
+// Generates random characters for each type selected by user
 // Gets lowercase letters
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
 // Gets uppercase letters
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-
 // Gets numbers
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-
 // Gets special characters
 function getRandomSpecial() {
     const specialChar = '!@#$%^&*()_+';
@@ -101,6 +81,26 @@ function generatePassword() {
     for (var i = 0; i < length; i++) {
         password += passGen(lowercase, uppercase, number, special);
     }
+}
+
+// Calls functions to create and display password when button is clicked
+function writePassword() {
+    password = '';
+    // Asks users the criteria questions and records answers
+    questions();
+
+    // Uses the information collected in Questions to generate a random password based on the selected criteria
+    generatePassword();
+
+    // Returns password to text area
+    passwordText.value = password;
+}
+
+function copyPassword() {
+    var copyText = document.getElementById('password');
+    copyText.select();
+    document.execCommand('copy');
+    alert('Copied the text: ' + copyText.value);
 }
 
 // Add event listener to generate button
